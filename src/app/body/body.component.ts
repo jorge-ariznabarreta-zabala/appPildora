9346945bac2930b9b62a30e8da93786ddcbb8beb
jorge-ariznabarreta-zabala <jorge.ariznabarreta.zabala@gmail.com>
2023-03-01 09:01:50 +0100

    Presentación

--- src/app/body/body.component.ts
+++ src/app/body/body.component.ts
@@ -3,8 +3,14 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-body',
   templateUrl: './body.component.html',
-  styleUrls: ['./body.component.css']
+  styleUrls: ['./body.component.css'],
 })
 export class BodyComponent {
-
+  title = 'Prueba Angular';
+  contador: number = 0;
+  par: boolean = false;
+  pulsaBoton() {
+    this.contador++;
+    this.par = this.contador % 2 == 0;
+  }
 }
